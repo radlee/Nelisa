30480f50327055cf78b30e736bf800d400931e6f
radlee <lolito@projectcodex.co>
2016-07-11 09:44:21 +0200

    Images

--- express-app/server.js
+++ express-app/server.js
@@ -46,7 +46,7 @@ app.get('/sales/:week_name', function(req, res){
 });
 
 //start the server
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
  var host = server.address().address;
  var port = server.address().port;
  console.log('App listening at http://%s:%s', host, port);
